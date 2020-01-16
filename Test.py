3d577027152b6624bb41ab15c4bd066e0e284300
Basharat Wani <basharatw@gmail.com>
2020-01-16 13:11:13 -0800

    Create Test.py
    Initial version

---
+++ Test.py
@@ -0,0 +1,20 @@
+print ("01 Jan 2020")
+print ("Test Hello World1")
+print ("Test Hello World2")
+print ("Test Hello World3")
+
+print  (2+2)
+print  (50-5*6)
+print  ((50 - 5*6) / 4)
+
+print ("# Division always returns a floating point number")
+print  ( 8 / 5)
+
+print (" # classic Division returns a float")
+print  (17/3)
+
+print ("# Floor division discards the fractional part")
+print  (17//3)
+
+print ("# the % operator returns the Remainder of the division")
+print  (17 % 3)
